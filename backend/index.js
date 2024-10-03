7e76c114d5a6be09c55509c8dbd1a5b9866a6916
arturmuursepp <artur.ateam@gmail.com>
2024-10-03 15:01:20 +0300

    Added nodemon

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,28 @@ const swaggerDoc = require("./docs/swagger.json")
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc))
 
 app.get("/", (req, res) => {
-    res.send(`Server running. Docs at <a href="https://${host}:${port}/docs">/docs</a>`)
+    res.send(`Server running. Docs at <a href="http://${host}:${port}/docs">/docs</a>`)
 })
 
+const games = [
+    { id:0, name:"Witcher 3", price:29.99},
+    { id:1, name:"Cyberpunk 2077", price:39.99},
+    { id:2, name:"Minecraft", price:29.99},
+    { id:3, name:"Counter-strike: Global offensive", price:0},
+    { id:4, name:"Roblox", price:29.99},
+    { id:5, name:"Grand theft Auto V", price:29.99},
+    { id:6, name:"Valorant", price:null},
+    { id:7, name:"Forza Horizon 5", price:59.99},
+]
+
 app.get("/games", (req, res) => {
-    res.send(["Witcher 3", "Cybepunk 2077"])
+    res.send(games.map(g => g.name))
 })
+
+app.get("/games/:id", (req, res) => {
+    res.send(games[req.params.id])
+})
+
 
 
 app.listen(port, () => {
