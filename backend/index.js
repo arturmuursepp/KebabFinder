a29085cdd0107556b02e0ffc49a1b873e730d573
arturmuursepp <artur.ateam@gmail.com>
2024-10-03 13:52:51 +0300

    first commit

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const port = 8080
+const host = "localhost"
+const app = require("express")()
+const swaggerUi = require("swagger-ui-express")
+const swaggerDoc = require("./docs/swagger.json")
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc))
+
+app.get("/", (req, res) => {
+    res.send(`Server running. Docs at <a href="https://${host}:${port}/docs">/docs</a>`)
+})
+
+app.get("/games", (req, res) => {
+    res.send(["Witcher 3", "Cybepunk 2077"])
+})
+
+
+app.listen(port, () => {
+    console.log(`API up at: http://localhost:${port}`)
+})
